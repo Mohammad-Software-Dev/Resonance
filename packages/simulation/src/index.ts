27b1c99c83633c24016e4e8c812e79fb88665877
Mohammad-Ahmad <144995811+Mohammad-Software-Dev@users.noreply.github.com>
2026-09-26 15:56:11 +0200

    m0.10: export replay contracts

--- packages/simulation/src/index.ts
+++ packages/simulation/src/index.ts
@@ -1,7 +1,7 @@
 import { SIM_DT_SECONDS, asTick, type Tick } from "@resonance/game-data";
 
 export * from "./hash";
-export * from "./input";
+export * from "./input";\nexport * from "./replay";
 export * from "./simulation";
 export * from "./state";
 
